8c5e56a3abe7d653a4ea30f5600402d93b2a6eda
GREESHMA SREEKUMAR <61485318+greeshma-sreekumar@users.noreply.github.com>
2022-01-22 01:19:19 +0530

    Update polygonal_number.py

--- polygonal_number.py
+++ polygonal_number.py
@@ -1,1 +1,12 @@
+# print polygonal numbers
+p = int(input("Enter the no.of edges of the polygon: "))
+n = int(input("Enter no.of terms needed: "))
+def printPoluNum(p,n):
+    for i in range(1, n):
+        m = ((p-2)*i*(i+1)/2) -((p-3)*i)
+        print(int(m), end=" ")
 
+
+printPoluNum(p,n)
+
+#find if a number is 
