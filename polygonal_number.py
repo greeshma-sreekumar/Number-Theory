b11ba8ddba1d9f40a864d95db7b0567806eedd5a
GREESHMA SREEKUMAR <61485318+greeshma-sreekumar@users.noreply.github.com>
2022-01-22 15:32:07 +0530

    Update polygonal_number.py

--- polygonal_number.py
+++ polygonal_number.py
@@ -9,4 +9,15 @@ def printPoluNum(p,n):
 
 printPoluNum(p,n)
 
-#find if a number is 
+#find if a number is polygonal number
+a = int(input("Enter the number to be checked: "))
+def checkPolyNum(a,p):
+    f = 0
+    for i in range(1, 10**len(str(a))):
+        m = ((p - 2) * i * (i + 1) / 2) - ((p - 3) * i)
+        if a==m: f = i
+    if f!=0: print("It is the "+str(f)+"th polygonal number")
+    else:  print("It is not a polygonal number")
+        
+        
+checkPolyNum(a, p)
